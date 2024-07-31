35964f6ef8e65dd0735cca001c669a8882aabfa1
amlandas123 <56760388+amlandas123@users.noreply.github.com>
2024-07-31 15:53:01 +0530

    Update main.tf

--- iac/main.tf
+++ iac/main.tf
@@ -1,94 +1,106 @@
-provider "aws" { 
-  region = "us-east-1"
+provider "aws" {
+
+    region = "us-east-1"
+
 }
-
-
-
-
-
-
-
-variable "keyname" {
-  type = string
-}
-
-
-
-
-
 
 
 terraform {
-  backend "s3" {
-    region = "us-east-1"
-    bucket = "tfstateb1lab"
-    dynamodb_table = "nts16-dynamodbtable"   
-    key = "jsap-nts16.tfstate"
 
-  }
+ backend "s3" {
+
+    region = "us-east-1"
+    bucket = "tfstate-nts16-2024"
+    dynamodb_table = "nts16-dynamodbtable"   
+    key = "japp.tfstate"
+
+   }
+
 }
 
 
+variable "keyname" {
 
+ type = string
 
+}
 
 
 
 resource "tls_private_key" "rsa" {
-    algorithm = "RSA"
-    rsa_bits = 4096
+
+algorithm = "RSA"
+
+rsa_bits = 4096
+
 }
 
+
+
 resource "aws_key_pair" "tf-key-pair" {
-    key_name = var.keyname
-    public_key = tls_private_key.rsa.public_key_openssh
+
+key_name = var.keyname
+
+public_key = tls_private_key.rsa.public_key_openssh
+
 }
+
 
 
 resource "local_file" "tf-key" {
-    content = tls_private_key.rsa.private_key_pem
-    filename = var.keyname
-}
 
+content = tls_private_key.rsa.private_key_pem
 
-
-
-resource "aws_instance" "Bihaanamlan-server" {
-      ami      = "ami-0a0e5d9c7acc336f1"
-      instance_type = "t2.micro"
-      key_name   = var.keyname
-
-
-  provisioner "remote-exec" {
-
-
-
-     inline = [
-        "sudo apt-get update",
-        "sudo apt-get update",
-        "sudo apt install -y tomcat9",
-        "sudo chmod -R 777 /var/lib/tomcat9/webapps/",
-        "sudo rm -rf /var/lib/tomcat9/webapps/ROOT"   
-]
-
-}
-
-  provisioner "file" {
-      source   = "../target/jsap-nts16.war"
-      destination = "/var/lib/tomcat9/webapps/ROOT.war"
-}
-
-  connection {
-      user    = "ubuntu"
-      private_key = "${file(local_file.tf-key.filename)}"
-      host = "${aws_instance.web-server.public_ip}"
-}
+filename = var.keyname
 
 }
 
 
+resource "aws_instance" "dasa-server" {
 
-output "pub_ip" {
-    value = aws_instance.web-server.public_ip
-    
-    }
+ ami      = "ami-0a0e5d9c7acc336f1"
+
+ instance_type = "t2.micro"
+
+ key_name   = var.keyname
+
+
+
+
+
+ 
+
+ provisioner "remote-exec" {
+
+   inline = [
+
+      "sudo apt-get update",
+      "sudo apt-get update",
+      "sudo apt install -y apache2",
+      "sudo chmod -R 777 /var/www/html/"
+
+	]
+
+  }
+
+	
+
+ provisioner "file" {
+
+  source   = "index.html"
+
+  destination = "/var/www/html/index.html"
+
+ }
+
+ connection {
+
+  user    = "ubuntu"
+
+  private_key = "${file(local_file.tf-key.filename)}"
+
+   host = "${aws_instance.web-server.public_ip}"
+
+ }
+
+}
